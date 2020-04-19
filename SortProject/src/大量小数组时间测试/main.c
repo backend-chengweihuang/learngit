897659c5887fa96cf0d8b10e8a147dabe0be3c11
backend-weihuang <2360495386@qq.com>
2020-04-19 21:19:04 +0800

    SortProject

---
+++ SortProject/src/大量小数组时间测试/main.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "qgsort.h"
+
+void randNum( int* a, int count);
+void timeTest(int *a,int size);
+
+int main()
+{
+    int a[100];
+    puts("在100k次数下100给数据排序时间如下:");
+    timeTest(a,100);
+    puts("按任意键退出。。。。");
+    getchar();
+}
+
+void randNum( int* a, int count )
+{
+
+    int i = 0;
+
+    while( i < count )
+    {
+
+        a[i++] = rand() % (count+1);
+
+    }
+
+}
+
+void timeTest(int *a,int size)
+{
+    int t[200000];
+    clock_t start, finish;
+
+	start = clock();
+	for(int i =0 ;i < 100000;i++)
+	{
+	    randNum(a,size);
+	    insertSort(a,size);
+	}
+	finish = clock();
+	printf("插入排序用时%ld ms\n",finish-start);
+
+	start = clock();
+	for(int i =0 ;i < 100000;i++)
+	{
+	    randNum(a,size);
+	    MergeSort(a,0,size-1,t);
+	}
+	finish = clock();
+	printf("归并排序用时%ld ms\n",finish-start);
+
+	start = clock();
+	for(int i =0 ;i < 100000;i++)
+	{
+	    randNum(a,size);
+	    QuickSort_Recursion(a, 0,size-1);
+	}
+	finish = clock();
+	printf("快速排序(递归版)用时%ld ms\n",finish-start);
+
+	start = clock();
+	for(int i =0 ;i < 100000;i++)
+	{
+	    randNum(a,size);
+	    QuickSort(a,size);
+	}
+	finish = clock();
+	printf("快速排序(非递归版)用时%ld ms\n",finish-start);
+
+	start = clock();
+	for(int i =0 ;i < 100000;i++)
+	{
+	    randNum(a,size);
+	    Partition(a, 0, size-1);
+	}
+	finish = clock();
+	printf("快速排序(枢轴存放)用时%ld ms\n",finish-start);
+
+	start = clock();
+	for(int i =0 ;i < 100000;i++)
+	{
+	    randNum(a,size);
+	    RadixCountSort(a,size);
+	}
+	finish = clock();
+	printf("基数排序用时%ld ms\n",finish-start);
+}
